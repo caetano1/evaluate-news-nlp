0ba1fe356af43d5bf96e0bd5b358808db9851b38
caetano <joaoluisfcaetano@gmail.com>
2020-10-24 19:12:05 -0300

    Adjusting the regex in the webpack dev config file

--- webpack.dev.js
+++ webpack.dev.js
@@ -15,12 +15,12 @@ module.exports = {
     module: {
         rules: [
             {
-                test: '/\.js$/',
+                test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
             },
             {
-                test: '/\.scss$/',
+                test: /\.scss$/,
                 use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
